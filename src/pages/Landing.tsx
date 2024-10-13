4bb01a0e14f2caa1fec9c326f5234593b61c3003
Sarthak Mishra <msarthak785@gmail.com>
2024-10-13 19:32:13 +0530

    responsive UI

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -7,14 +7,15 @@ export const Landing = () => {
     // create routes
     // add landing page content
     // polish landing page
+    // add responsiveness
     // add firebase login
     // add content to dashboard
 
     return(
-        <div className="py-20 px-60 text-center space-y-12">
+        <div className="py-20 px-20 xl:px-60 text-center space-y-12">
             <div className="space-y-4">
-                <p className="text-8xl font-bold leading-tight tracking-tighter">Destion Innovations</p>
-                <p className="text-xl leading-snug">Destion: Innovating with Passion and Expertise. Led by a dynamic team of enthusiastic students and developers, Destion specializes in web development, Android development, and data science. We turn visionary ideas into powerful digital solutions, driving the future of technology.</p>
+                <p className="text-4xl md:text-6xl lg:text-8xl font-bold leading-tight tracking-tighter">Destion Innovations</p>
+                <p className="text-left xl:text-center text-md md:text-lg lg:text-xl leading-snug">Destion: Innovating with Passion and Expertise. Led by a dynamic team of enthusiastic students and developers, Destion specializes in web development, Android development, and data science. We turn visionary ideas into powerful digital solutions, driving the future of technology.</p>
             </div>
             <div>
                 <Link
--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,40 +1,43 @@
 import { Link } from "react-router-dom";
 
 export const Signup = () => {
+    const handleSubmit = (e: React.SyntheticEvent) => {
+        e.preventDefault();
+    };
     return(
-        <div className="py-20 px-60 text-center space-y-6">
+        <div className="py-20 xl:px-60 text-center space-y-6">
             <form className="flex flex-col items-center space-y-4">
                 <input
                     type="text"
                     required
                     placeholder="First Name"
-                    className="border-b-2 border-gray-600 p-1"
+                    className="sm:w-[25%] border-b-2 border-gray-600 p-1"
                 />
                 <input
                     type="text"
                     required
                     placeholder="Last Name"
-                    className="border-b-2 border-gray-600 p-1"
+                    className="sm:w-[25%] border-b-2 border-gray-600 p-1"
                 />
                 <input
                     type="email"
                     required
                     placeholder="Email"
-                    className="border-b-2 border-gray-600 p-1"
+                    className="sm:w-[25%] border-b-2 border-gray-600 p-1"
                 />
                 <input
                     type="password"
                     required
                     placeholder="Password"
-                    className="border-b-2 border-gray-600 p-1"
+                    className="sm:w-[25%] border-b-2 border-gray-600 p-1"
                 />
                 <input
                     type="password"
                     required
                     placeholder="Confirm Password"
-                    className="border-b-2 border-gray-600 p-1"
+                    className="sm:w-[25%] border-b-2 border-gray-600 p-1"
                 />
-                <input className="cursor-pointer font-semibold p-2 border-2 border-gray-600 rounded-sm transition-all bg-black text-white hover:bg-white hover:text-black" type="submit" />
+                <input onSubmit={ handleSubmit } className="sm:w-[25%] cursor-pointer font-semibold p-2 border-2 border-gray-600 rounded-sm transition-all bg-black text-white hover:bg-white hover:text-black" type="submit" />
             </form>
             <div>
                 <Link to="/login" className="text-lg font-semibold hover:underline">
